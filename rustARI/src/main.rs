cc36b76d384823c5cdaab6b456d708582eb10dfe
Rasengangstarr <davidouthere@msn.com>
2020-08-03 18:27:10 +0100

    implemented file writing

--- rustARI/src/main.rs
+++ rustARI/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
+mod rom_read;
+
 fn main() {
-    println!("Hello, world!");
+   let args: Vec<String> = env::args().collect();
+    assert_eq!(args.len(), 2, "wrong number of arguments provided! provide a filename only");
+    let filename = &args[1];
+    println!("reading file: {}", filename);
+    let rom = rom_read::get_file_as_byte_vec(filename);
+    for (x,byte) in rom.iter().enumerate() {
+       println!("{}:{:#X?}", x, byte)
+    }
 }
+
