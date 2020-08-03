42ee5d45ea968acf87efd93f0e1307661a1af616
Rasengangstarr <davidouthere@msn.com>
2020-08-03 21:47:19 +0100

    implemented a couple of instructions

--- rustARI/src/main.rs
+++ rustARI/src/main.rs
@@ -13,12 +13,34 @@ fn main() {
    
    println!("reading file: {}", filename);
 
-   let rom = rom_read::get_file_as_byte_vec(filename);
-   let mut mem : [u8; 0x1FFF]  = mem_load::write_rom_to_mem(rom);
-
+   let mut flags : u8 = 0x0;
    
+   let rom = rom_read::get_file_as_byte_vec(filename);
+   let mem : [u8; 0x1FFF]  = mem_load::write_rom_to_mem(rom);
+
+   let pc = 0x1000;
+   
+   match mem[pc] {
+      0x78 => flags = sei(&flags),
+      0xD8 => flags = cld(&flags),
+      _ => println!("not an instruction"),
+   }
+
+   println!("{:#X?}",mem[pc]);
    
 }
 
+fn sei(flags: &u8) -> u8 {
+   println!("sei");
+   let mut result = *flags;
+   result &= !0b0000_0100;
+   return result;
+}
 
+fn cld(flags: &u8) -> u8 {
+   println!("cld");
+   let mut result = *flags;
+   result &= !0b0000_1000;
+   return result;
+}
 
